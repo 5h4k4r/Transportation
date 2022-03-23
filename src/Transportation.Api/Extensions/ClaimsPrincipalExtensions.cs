340c5ffcf9c895acca6ecb4d0bd1abca2ccad7ce
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-03-23 15:12:03 +0300

    Updated Principal

--- src/Transportation.Api/Extensions/ClaimsPrincipalExtensions.cs
+++ src/Transportation.Api/Extensions/ClaimsPrincipalExtensions.cs
@@ -6,19 +6,6 @@ namespace BabyCareApi.Extensions;
 
 public static class ClaimsPrincipalExtensions
 {
-    private static transportationContext context = new transportationContext();
-    public static IEnumerable<Role> GetRole(this ClaimsPrincipal user)
-    {
-        var roleId = user.FindFirstValue(ClaimTypes.Role);
-
-        if (string.IsNullOrEmpty(roleId))
-            return null;
-
-        return context.Roles.Where(role => role.Id == Int32.Parse(roleId));
-
-
-    }
-
     public static string GetJti(this ClaimsPrincipal user)
             => user.FindFirstValue("jti");
 
