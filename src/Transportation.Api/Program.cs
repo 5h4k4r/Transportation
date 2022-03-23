9d1ac9a03951ffb5f0dc5922e4abc6afc57aa979
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-03-23 15:12:15 +0300

    Updated Program.cs

--- src/Transportation.Api/Program.cs
+++ src/Transportation.Api/Program.cs
@@ -1,19 +1,44 @@
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
 using Payroll.PaygridApi.Helpers;
 using Transportation.Api;
+using Transportation.Api.Helpers;
+using Transportation.Api.Model;
+using Transportation.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
 // Add services to the container.
 
-builder.Services.AddControllers()
-.AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services
+    .AddControllers()
+    .AddJsonOptions(x =>
+    {
+        x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        x.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
 
-builder.Services.AddAuthentication().AddScheme<UserAuthOptions, GatewayAuthHandler>("Basic", null);
-builder.Services.AddSingleton<GatewayAuthHandler>();
+        x.JsonSerializerOptions.PropertyNamingPolicy = SnakeCaseNamingPolicy.Default;
+        x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+services
+    .AddScoped<UserAuthContext>()
+    .AddAuthentication(x =>
+    {
+        x.DefaultChallengeScheme = "Basic";
+        x.DefaultAuthenticateScheme = "Basic";
+    })
+    .AddScheme<UserAuthOptions, GatewayAuthHandler>("Basic", null);
+
+// services.AddSingleton<GatewayAuthHandler>();
+services.AddDbContext<transportationContext>(x => x.UseMySql(builder.Configuration["MariaDb:ConnectionString"], Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.5.15-mariadb")));
+
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
@@ -23,8 +48,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
