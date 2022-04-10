dcdbd930844d1adec8402e3d33ddf88e280012d1
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-04-10 10:23:56 +0300

    Added ConfigureRepositoryWrapper Extension method

---
+++ src/Transportation.Api/Extensions/ConfigureRepositoryWrapper.cs
@@ -0,0 +1,14 @@
+using Repository;
+using Transportation.Api.Interfaces;
+using Transportation.Api.Repositories;
+
+namespace Transportation.Api.Extensions;
+
+public static partial class RepositoryWrapperExtension
+{
+    public static IServiceCollection ConfigureRepositoryWrapper(this IServiceCollection services)
+    {
+        services.AddTransient<IUnitOfWork, UnitOfWork>();
+        return services;
+    }
+}
