d0f450fee0bdd92815b2f74a2773689a3f2bf26b
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-04-12 10:34:44 +0300

    Add DateExtension

---
+++ src/Transportation.Api/Extensions/DateExtensions.cs
@@ -0,0 +1,14 @@
+namespace Transportation.Api.Extensions;
+
+public static class DateExtensions
+{
+    public static DateTime StartOfDay(this DateTime theDate)
+    {
+        return theDate.Date;
+    }
+    public static DateTime EndOfDay(this DateTime theDate)
+    {
+        return theDate.Date.AddDays(1).AddTicks(-1);
+    }
+
+}
