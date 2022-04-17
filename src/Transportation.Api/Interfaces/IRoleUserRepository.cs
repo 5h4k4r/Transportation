5f8d04da6a52b4c62f8d718d02d412af6b9c4bec
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-04-17 12:00:05 +0300

    Add RoleUserRepository

---
+++ src/Transportation.Api/Interfaces/IRoleUserRepository.cs
@@ -0,0 +1,8 @@
+using Transportation.Api.Model;
+
+namespace Tranportation.Api.Repositories;
+
+
+public interface IRoleUserRepository
+{
+}
---
+++ src/Transportation.Api/Repositories/RoleUserRepository.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Tranportation.Api.Repositories;
+using Transportation.Api.Model;
+
+namespace Transportation.Api.Repositories;
+
+public class RoleUserRepository : IRoleUserRepository
+{
+
+    private readonly transportationContext _context;
+
+    public RoleUserRepository(transportationContext context)
+    {
+        _context = context;
+    }
+
+
+}
