3f118ad4d0eb38e141f0930489c6b72d5f66a315
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-04-19 10:27:39 +0300

    Add Validation for requests

--- src/Transportation.Api/Models/Requests/ListTasksRequest.cs
+++ src/Transportation.Api/Models/Requests/ListTasksRequest.cs
@@ -6,17 +6,30 @@ using Transportation.Api.Interfaces;
 namespace Tranportation.Api.Requests;
 
 
-public class ListTasksRequest : IPagingOptions, ISortOptions
+public class ListTasksRequest : IPagingOptions, ISortOptions, IValidatableObject
 {
     [Required]
-    public ulong AreaId { get; set; }
+    public ulong? AreaId { get; set; }
     public DateTime StartAt { get; set; } = DateTime.Today;
     public DateTime EndAt { get; set; } = DateTime.Today;
     public TaskState? Status { get; set; } = null;
-
+    [AllowedValues(
+        "Task.Id", "Task.Price",
+        "Task.Tip", "Task.Status",
+        "Task.CreatedAt", "Task.UpdatedAt",
+        "Task.RequestId", "Destination.Distance",
+        "Destination.Duration"
+    )]
     public string? SortField { get; set; }
     public bool? SortDescending { get; set; } = false;
     public int? Page { get; set; } = 0;
     public int? Limit { get; set; } = Constants.DefaultPaginationLimit;
 
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (AreaId is null)
+            yield return new ValidationResult($"The field {nameof(AreaId)} is required", new[] { nameof(AreaId) });
+    }
+
 }
+
