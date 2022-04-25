1e88a8ec604c8ab76922011be57dfc33ad5e94ad
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-04-25 15:38:54 +0300

    Add Employees Controller

--- src/Transportation.Api/Interfaces/IEmployees.cs
+++ src/Transportation.Api/Interfaces/IEmployees.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Tranportation.Api.Requests;
 using Transportation.Api.Model;
 
+using Task = System.Threading.Tasks.Task;
 namespace Tranportation.Api.Repositories;
 
 
 public interface IEmployeesRepository
 {
     Task<Employee?> GetEmployeeByUserId(ulong Id);
+    EntityEntry<Employee> ChangeEmployeeLanguage(ChangeEmployeeLanguageRequest model);
 }
