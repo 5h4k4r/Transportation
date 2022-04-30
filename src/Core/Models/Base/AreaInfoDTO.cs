1abd818e07e2f88c7babe85b8be2bf632ec6c7b4
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-04-30 15:18:47 +0300

    fix indentation

--- src/Core/Models/Base/AreaInfoDTO.cs
+++ src/Core/Models/Base/AreaInfoDTO.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 
 namespace Core.Models;
-    public partial class AreaInfoDTO
-    {
+public partial class AreaInfoDTO
+{
 
-        public ulong Id { get; set; }
-        public string AreaId { get; set; } = null!;
-        public string Title { get; set; } = null!;
-        public string Type { get; set; } = null!;
-        public string? Currency { get; set; }
-        public string? Country { get; set; }
-        public string? Timezone { get; set; }
-        public string? Center { get; set; }
-        public string? Bound { get; set; }
-        public DateTime? CreatedAt { get; set; }
- 
+    public ulong Id { get; set; }
+    public string AreaId { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Type { get; set; } = null!;
+    public string? Currency { get; set; }
+    public string? Country { get; set; }
+    public string? Timezone { get; set; }
+    public string? Center { get; set; }
+    public string? Bound { get; set; }
+    public DateTime? CreatedAt { get; set; }
+
 }
