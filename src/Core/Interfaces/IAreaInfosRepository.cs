d53fb36d82d74b6e0ebf194aeb3d3f967a00b9ee
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-04-30 15:18:23 +0300

    Core -> interfaces Update IAreaInfosRepository

--- src/Core/Interfaces/IAreaInfosRepository.cs
+++ src/Core/Interfaces/IAreaInfosRepository.cs
@@ -4,7 +4,7 @@ namespace Core.Interfaces;
 
 public interface IAreaInfosRepository
 {
-    Task<List<AreaInfoDTO>> GetAreaInfoById(ulong Id);
-    Task<List<AreaInfoDTO>> GetAreaInfoByTitle(string Title);
+    Task<AreaInfoDTO?> GetAreaInfoById(ulong Id);
+    Task<AreaInfoDTO?> GetAreaInfoByTitle(string Title);
     Task<List<AreaInfoDTO>> ListAreaInfos();
 }
