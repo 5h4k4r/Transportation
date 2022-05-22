b88d48ba37bf864ff0334c2497c72d46c9abaa30
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-05-22 15:08:02 +0300

    Update ServantsController: Add GetServantById Add ListServants

---
+++ src/Core/Models/Requests/ListServantRequest.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using Core.Interfaces;
+
+namespace Core.Requests;
+
+public class ListServantRequest : IPagingOptions, IValidatableObject
+{
+    /// <summary>
+    /// Allowed Values: "Name, NationalId, PhoneNumber"
+    /// </summary>
+    [AllowedValues("Name, NationalId, PhoneNumber")]
+    public string? SearchField { get; set; }
+    public string? SearchValue { get; set; }
+
+    public int? Page { get; set; } = 0;
+
+    [Range(0, Constants.MaxPaginationLimit)]
+    public int? Limit { get; set; } = Constants.DefaultPaginationLimit;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (SearchField == null && SearchValue != null)
+            yield return new ValidationResult($"The field {nameof(SearchField)} is required", new[] { nameof(SearchField) });
+
+        else if (SearchField != null && SearchValue == null)
+            yield return new ValidationResult($"The field {nameof(SearchValue)} is required", new[] { nameof(SearchValue) });
+    }
+}
