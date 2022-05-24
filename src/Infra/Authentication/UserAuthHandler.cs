e5a25e2072e7bab7ca7a1fd29646ed0bbf901ce0
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-05-24 17:03:17 +0300

    Update Authentication: Add Custom Authorize Attribute to authorize users by their roles

--- src/Infra/Authentication/UserAuthHandler.cs
+++ src/Infra/Authentication/UserAuthHandler.cs
@@ -1,10 +1,14 @@
 using System.Security.Claims;
 using System.Text.Encodings.Web;
 using System.Text.Json;
+using Core.Auth.Models;
+using Core.Interfaces;
+using Infra.Entities;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Transportation.Api.Auth;
+using Task = System.Threading.Tasks.Task;
 
 namespace Core.Helpers;
 
@@ -14,27 +18,30 @@ public class UserAuthHandler : AuthenticationHandler<UserAuthOptions>
     private readonly IOptionsMonitor<UserAuthOptions> _Options;
     private readonly JsonSerializerOptions _JsonSerializerOptions;
     private readonly UserAuthContext _UserAuthContext;
+    private readonly IUnitOfWork _unitOfWork;
 
     public UserAuthHandler(
         UrlEncoder encoder,
         UserAuthContext authContext,
         IOptionsMonitor<UserAuthOptions> options,
         ILoggerFactory logger,
-        ISystemClock clock) : base(options, logger, encoder, clock)
+        ISystemClock clock,
+        IUnitOfWork unitOfWork
+        ) : base(options, logger, encoder, clock)
     {
         _Options = options;
         _UserAuthContext = authContext;
-
+        _unitOfWork = unitOfWork;
         _JsonSerializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
     }
 
-    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
     {
         var auth = Request.Headers["user"].FirstOrDefault();
 
         //  TODO: when the header is empty the returned result is empty
         if (string.IsNullOrEmpty(auth))
-            return Task.FromResult(AuthenticateResult.Fail("No Header is found"));
+            return AuthenticateResult.Fail("No Header is found");
 
         try
         {
@@ -42,24 +49,29 @@ public class UserAuthHandler : AuthenticationHandler<UserAuthOptions>
 
             if (resp is not AuthUser user)
                 // return AuthenticateResult.Fail(new GatewayAuthException(GatewayAuthException.ErrorCode.InvalidUserModel, "User ID is empty"));
-                return Task.FromResult(AuthenticateResult.Fail(new NotImplementedException()));
+                return AuthenticateResult.Fail(new NotImplementedException());
 
             //     await _UserService.UpsertAsync(user);
 
             //     // TODO: Add fake accounting unit
             //     resp.Data.Attributes.AccountingUnit = new AccountingUnit { Id = "YsrbjgBLha1aUYV4svFR8" };
+            var databaseUser = await _unitOfWork.User.GetUserByAuthId(resp.Id);
+            if (databaseUser is null)
+                return AuthenticateResult.Fail("User not found");
+
+            resp.RoleUsers = databaseUser.RoleUsers.Select(x=>x.RoleId);
 
             _UserAuthContext.SetAuthUser(resp);
 
             var claimsIdentity = new ClaimsIdentity(GenerateClaims(user), nameof(UserAuthHandler));
             var ticket = new AuthenticationTicket(new ClaimsPrincipal(claimsIdentity), Scheme.Name);
-            return Task.FromResult(AuthenticateResult.Success(ticket));
+            return AuthenticateResult.Success(ticket);
         }
         catch (JsonException e)
         {
             //     var authException = new GatewayAuthException(GatewayAuthException.ErrorCode.InvalidUserModel, e.Message);
             // return AuthenticateResult.Fail(authException);
-            return Task.FromResult(AuthenticateResult.Fail(e));
+            return AuthenticateResult.Fail(e);
         }
 
     }
@@ -85,8 +97,13 @@ public class UserAuthHandler : AuthenticationHandler<UserAuthOptions>
         var claims = new List<Claim>
         {
             new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-            new Claim(ClaimTypes.MobilePhone, user.Mobile)
+            new Claim(ClaimTypes.MobilePhone, user.Mobile),
+
+
         };
+
+        if (user.RoleUsers is not null)
+            claims.Add(new Claim(ClaimTypes.Role, JsonSerializer.Serialize(user.RoleUsers)!));
 
 
         return claims;
