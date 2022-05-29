4ec6cccfe1c68f2a655e954acfddde3c723a11ed
Arin Faraj <arin.abdul99@gmail.com>
2022-05-29 09:37:30 +0300

    improve RedisCacheRepository by adding "using" to GetClientAsync()

--- src/Infra/Repositories/RedisCacheRepository.cs
+++ src/Infra/Repositories/RedisCacheRepository.cs
@@ -23,90 +23,90 @@ public class RedisCacheRepository : ICacheRepository
 
     public async Task RemoveLocation(string key, string member)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
         await client.RemoveItemFromSortedSetAsync(key, member);
     }
 
     public async Task<bool> SetKey(string key, string value, TimeSpan timeToLive)
     {
         // TODO: Sliding expire time
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
         return await client.SetAsync(key, value, timeToLive);
     }
 
     public async Task<T> GetKey<T>(string key)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         return await client.GetAsync<T>(key);
     }
 
     public async Task<bool> DeleteKey(string key)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         return await client.RemoveAsync(key);
     }
 
     public async Task<long> AddLocation(string key, string member, ulong lat, ulong lng)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         return await client.AddGeoMemberAsync(key, lng, lat, member);
     }
 
     public async Task<double> GetDistance(string key, string member1, string member2, string unit = RedisGeoUnit.Meters)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         return await client.CalculateDistanceBetweenGeoMembersAsync(key, member1, member2, unit);
     }
 
     public async Task AddList(string listId, string value)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         await client.PushItemToListAsync(listId, value);
     }
 
     public async Task<long> GetListLen(string listId)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         return await client.GetListCountAsync(listId);
     }
 
     public async Task<long> Increase(string key, int value = 1)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         return await client.IncrementValueByAsync(key, value);
     }
 
     public async Task Increment(string key, int value = 1, TimeSpan timeToLive = default)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         throw new NotImplementedException();
     }
 
     public async Task<IEnumerable<string>> GetList(string listName, int start, int length)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         return await client.GetRangeFromListAsync(listName, start, length);
     }
 
     public async Task<long> RemoveFromList(string listName, string member)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         return await client.RemoveItemFromListAsync(listName, member);
     }
 
     public async Task<Position?> GetPosition(string key, string member)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         var pos = await client.GetGeoCoordinatesAsync(key, member);
 
@@ -121,14 +121,14 @@ public class RedisCacheRepository : ICacheRepository
 
     public async Task<IEnumerable<string>> GetRadius(string key, ulong lat, ulong lng, int radius)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         return await client.FindGeoMembersInRadiusAsync(key, lat, lng, radius, RedisGeoUnit.Meters);
     }
 
     public async Task<IEnumerable<string>?> GetLastPositionOnTask(string listName)
     {
-        var client = await _cache.GetClientAsync();
+        await using var client = await _cache.GetClientAsync();
 
         var items = await client.GetAllItemsFromListAsync(listName);
         return items?[0].Split('_');
