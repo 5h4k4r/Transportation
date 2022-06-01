3cb3ba173749c853ebe6c954aecac469a784b0df
syar1122 <syars66@gmail.com>
2022-06-01 16:23:51 +0300

    fix type error in AddServantToVehicle caused by the merge

--- src/Core/Models/Requests/AddServantToVehicleRequest.cs
+++ src/Core/Models/Requests/AddServantToVehicleRequest.cs
@@ -3,5 +3,5 @@ namespace Core.Models.Requests;
 public class AddServantToVehicleRequest
 {
     public ulong VehicleId { get; set; }
-    public int UserId { get; set; }
+    public ulong UserId { get; set; }
 }
