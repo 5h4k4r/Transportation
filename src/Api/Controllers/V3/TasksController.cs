04cba7e23e017db7b978f0302c69861d77f89d9f
syar1122 <syars66@gmail.com>
2022-06-06 16:33:31 +0300

    fix conflicts in TasksController and TasksRepository

--- src/Api/Controllers/V3/TasksController.cs
+++ src/Api/Controllers/V3/TasksController.cs
@@ -1,5 +1,8 @@
 using System.Net.Mime;
+using Api.Extensions;
+using Core.Models.Authentication;
 using Core.Models.Common;
+using Core.Models.Exceptions;
 using Core.Models.Repositories;
 using Core.Models.Requests;
 using Infra.Authentication;
@@ -8,7 +11,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers.V3;
-
 
 [Authorize]
 [ApiController]
@@ -25,32 +27,31 @@ public class TasksController : ControllerBase
     }
 
     /// <summary>
-    /// Lists all the tasks.
+    ///     Lists all the tasks.
     /// </summary>
-
     [HttpGet]
     [ProducesResponseType(typeof(BasicResponse), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(PaginatedResponse<ListTasks>), StatusCodes.Status200OK)]
-    public async Task<IActionResult> ListTasks([FromQuery] ListTasksRequest model, [FromServices] UserAuthContext authContext)
+    public async Task<IActionResult> ListTasks([FromQuery] ListTasksRequest model,
+        [FromServices] UserAuthContext authContext)
     {
-
         if (!User.HasRole(Role.SuperAdmin) && !User.GetAreaId().HasValue)
             throw new UnauthorizedException();
-        
+
         model.AreaId = User.GetAreaId().Value;
 
-        if ((await _unitOfWork.RoleUsers.GetRoleUserByUserId(user.Id))?.RoleId < 5)
-            model.AreaId = user.AreaId.Value;
+        // if ((await _unitOfWork.RoleUsers.GetRoleUserByUserId(user.Id))?.RoleId < 5)
+        //     model.AreaId = user.AreaId.Value;
 
         var items = await _unitOfWork.Tasks.ListTasks(model);
         var count = await _unitOfWork.Tasks.CountTasks(model);
 
         return Ok(new PaginatedResponse<ListTasks>(count, model, items));
     }
-    /// <summary>
-    /// Lists all tasks by a client.
-    /// </summary>
 
+    /// <summary>
+    ///     Lists all tasks by a client.
+    /// </summary>
     [HttpGet("client")]
     [ProducesResponseType(typeof(BasicResponse), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(PaginatedResponse<ListTasksByClient>), StatusCodes.Status200OK)]
