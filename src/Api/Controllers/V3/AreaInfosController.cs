592301cdbfdd933e229d2f7744d58f0386b18fc4
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-06-22 16:22:28 +0300

    testing pipelines

--- src/Api/Controllers/V3/AreaInfosController.cs
+++ src/Api/Controllers/V3/AreaInfosController.cs
@@ -48,6 +48,7 @@ public class AreaInfosController : ControllerBase
 
         else
         {
+            
             // TODO: this can be done with a join with area_infos table if area_id in employee table was foreign key
 
             var employee = await _unitOfWork.Employees.GetEmployeeByUserId(mySqlUser.Id);
