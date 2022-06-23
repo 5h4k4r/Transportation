3b4df43ce77b4c9a3ca0ac243f131ff7927b381f
Shakar Bakr <shakar.bakr@kubakgroup.com>
2022-06-23 10:10:37 +0300

    Update UpdateDocumentsRequest.cs

--- src/Core/Models/Requests/UpdateDocumentsRequest.cs
+++ src/Core/Models/Requests/UpdateDocumentsRequest.cs
@@ -6,13 +6,13 @@ public class UpdateDocumentsRequest
 {
     public string? Path { get; set; } = null!;
 
-    [JsonIgnore] public string Type { get; set; } = null!;
+    [JsonIgnore] public string? Type { get; set; } = null!;
 
     [JsonIgnore] public bool IsVerified { get; set; }
 
-    [JsonIgnore] public string ModelType { get; set; } = null!;
+    [JsonIgnore] public string? ModelType { get; set; } = null!;
 
-    [JsonIgnore] public ulong ModelId { get; set; }
+    [JsonIgnore] public ulong? ModelId { get; set; }
 
     [JsonIgnore] public DateTime? UpdatedAt { get; set; }
 }
